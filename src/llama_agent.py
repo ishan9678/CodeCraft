ea52c9f63f69c68bcfa0519f8323a2054fe87880
ishan <ishan.s@kalvium.com>
2025-01-12 16:03:56 +0530

    boilerplate llama3.3

--- src/llama_agent.py
+++ src/llama_agent.py
@@ -1,27 +1,17 @@
-# Import necessary libraries
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
-
 import os
+import openai
 
-os.environ.get('HF_TOKEN')
+client = openai.OpenAI(
+    api_key='',
+    base_url="https://api.sambanova.ai/v1",
+)
 
-# Hugging Face model name
-model_name = "meta-llama/Llama-3.2-1B"
+response = client.chat.completions.create(
+    model='Meta-Llama-3.3-70B-Instruct',
+    messages=[{"role":"system","content":"You are a helpful assistant"},{"role":"user","content":"Hello"}],
+    temperature =  0.1,
+    top_p = 0.1
+)
 
-# Load the tokenizer and model from Hugging Face model hub
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
-
-# Initialize the pipeline for text generation using the Llama model on CPU
-pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
-
-# Define a prompt for the model
-prompt = "Once upon a time in a small village"
-
-# Generate text based on the prompt using the Llama model
-generated_texts = pipe(prompt, max_length=100, num_return_sequences=1)
-
-# Print the generated output
-for i, result in enumerate(generated_texts):
-    print(f"Generated Text {i+1}: {result['generated_text']}")
-
+print(response.choices[0].message.content)
+      
